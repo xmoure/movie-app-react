5af5d899e8d27e8eea13db9546d36071ba927dc7
xmoure <ximenamoure@gmail.com>
2025-02-13 18:02:48 -0300

    finished

---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+const Search = ({ searchTerm, setSearchTerm }) => {
+
+    return (
+        <div className='search'>
+            <div>
+                <img src="search.svg" alt="Search"/>
+                <input
+                    type='text'
+                    placeholder='Search through thousands of movies'
+                    value={searchTerm}
+                    onChange={(e)=> setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Search
